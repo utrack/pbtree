8b329da9b19d44f742410032d156b7e8604a9fa0
Koptelov Nikita <nkoptelov@pushwoosh.com>
2020-09-06 18:26:16 +0300

    cmd: pbtree map added

--- map.go
+++ map.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
+	"github.com/utrack/pbtree/pbmap"
 )
 
 var Map = &cli.Command{
@@ -19,6 +22,8 @@ Mapping works recursively, so any project (B) that imports the current one (A) w
 A's imports the same way as A does.
 
 Map is written to the 'pbmap.yaml' at the project's root.
+
+Single wildcard expansion is supported; mapping '/foo/bar/*' to 'p.roj/repo!/foo/bar/*' works.
 `,
 	Category: "configuration",
 	Flags:    []cli.Flag{},
@@ -29,6 +34,26 @@ Map is written to the 'pbmap.yaml' at the project's root.
 		if iOrig == "" || iRepl == "" {
 			return errors.New("two import paths required, see pbtree help map")
 		}
-		return errors.New("not implemented")
+
+		mapFile := "pbmap.yaml"
+		in, err := os.Open(mapFile)
+		if err != nil {
+			return errors.Wrapf(err, "opening '%v' for reading", mapFile)
+		}
+
+		m, err := pbmap.Read(in)
+		_ = in.Close()
+		if err != nil {
+			return errors.Wrap(err, "reading pbmap")
+		}
+		m[iOrig] = iRepl
+
+		out, err := os.OpenFile(mapFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return errors.Wrapf(err, "re-opening '%v' for writing", mapFile)
+		}
+		defer out.Close()
+
+		return pbmap.Write(out, m)
 	},
 }
--- pbmap/pbmap.go
+++ pbmap/pbmap.go
@@ -4,17 +4,39 @@ package pbmap
 import (
 	"io"
 
+	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
 )
 
 type pbmap struct {
-	Version  string            `yaml:"version"`
+	Version  int               `yaml:"version"`
 	Replaces map[string]string `yaml:"replace"`
 }
 
+const maxMapVersion = 1
+
 func Read(r io.Reader) (map[string]string, error) {
 	var c pbmap
-	return c.Replaces, yaml.NewDecoder(r).Decode(&c)
+	err := yaml.NewDecoder(r).Decode(&c)
+	if err != nil {
+		return nil, err
+	}
+	if c.Version > maxMapVersion {
+		return nil, errors.Errorf("pbmap version '%v' is unsupported - max '%v'", c.Version, 1)
+	}
+
+	return c.Replaces, nil
 }
 
-// TODO Write(io.Writer)
+func Write(w io.Writer, m map[string]string) error {
+	var c pbmap
+	c.Version = maxMapVersion
+	c.Replaces = m
+	enc := yaml.NewEncoder(w)
+
+	err := enc.Encode(c)
+	if err != nil {
+		return err
+	}
+	return enc.Close()
+}
