459942ee424bac13673ded1e78b9ebe9c855dc38
Koptelov Nikita <nkoptelov@pushwoosh.com>
2020-09-10 17:59:20 +0300

    build_tree: fixed nilptr panic when there's no directories to scan

--- app/build_tree.go
+++ app/build_tree.go
@@ -40,6 +40,9 @@ func BuildTree(ctx context.Context, c Config) error {
 		}
 
 		filepath.Walk(absPath, func(p string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
 			if info.IsDir() {
 				return nil
 			}
